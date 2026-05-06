b30f74190f060410c0cb2235188c6337c10b141e
raju barman <rajubarman7665@gmail.com>
2026-05-06 15:50:26 +0530

    3d model added

--- wolf_threejs_project/src/components/Dog.jsx
+++ wolf_threejs_project/src/components/Dog.jsx
@@ -1,14 +1,18 @@
-import React from "react";
 import { useThree } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
 const Dog = () => {
-  useThree(({ camera, scene, gl }) => {
-    console.log(camera.position);
+  const { scene } = useGLTF("/models/dog.drc.glb");
+  useThree(({ camera, scene, gl })=>{
+    camera.position.z = 2;
+    
   });
   return (
-    <mesh>
-      <meshBasicMaterial color={0x00ff00} />
-      <boxGeometry args={[1, 1, 1]} />
-    </mesh>
+    <>
+      <primitive object={scene} position={[0, 0, 0]}  />
+      <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
+      <OrbitControls />
+    </>
   );
 };
 
