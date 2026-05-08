0e40ffc131fd081cb7e86f6805578e5390ee4da5
raju barman <rajubarman7665@gmail.com>
2026-05-08 19:41:15 +0530

    fix some bugs

--- wolf_threejs_project/src/components/Dog.jsx
+++ wolf_threejs_project/src/components/Dog.jsx
@@ -1,44 +1,51 @@
 import * as THREE from "three";
 import { useThree } from "@react-three/fiber";
-import {
-  OrbitControls,
-  useGLTF,
-  useTexture,
-  useAnimations,
-} from "@react-three/drei";
+import { useGLTF, useTexture, useAnimations } from "@react-three/drei";
 import { useEffect } from "react";
 
 const Dog = () => {
-  const model  = useGLTF("/models/dog.drc.glb");
+  const model = useGLTF("/models/dog.drc.glb");
 
   useThree(({ camera, scene, gl }) => {
-    camera.position.z = 1;
+    camera.position.z = 0.7;
     gl.toneMapping = THREE.ReinhardToneMapping;
     gl.outputColorSpace = THREE.SRGBColorSpace;
   });
   const { actions } = useAnimations(model.animations, model.scene);
 
   useEffect(() => {
-    actions[ "Take 001" ].play()
+    actions["Take 001"].play();
   }, [actions]);
 
-  
-
-  const [normalMap, sampleCap] = useTexture([
+  const [normalMap, sampleMatCap] = (useTexture([
     "/dog_normals.jpg",
     "/matcap/mat-2.png",
-  ]).map((texture) => {
+  ])).map(texture => {
     texture.flipY = false;
     texture.colorSpace = THREE.SRGBColorSpace;
     return texture;
   });
+  const [branchMap, branchNormalMap] = (useTexture([
+    "/branches_diffuse.jpeg",
+    "/branches_normals.jpeg",
+  ])).map(texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  });
+  const dogMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: normalMap,
+    matcap: sampleMatCap,
+  });
+  const branchMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: branchNormalMap,
+    matcap: branchMap,
+  });
 
   model.scene.traverse((child) => {
     if (child.name.includes("DOG")) {
-      child.material = new THREE.MeshMatcapMaterial({
-        normalMap: normalMap,
-        matcap: sampleCap,
-      });
+      child.material = dogMaterial;
+    } else {
+      child.material = branchMaterial;
     }
   });
 
@@ -47,10 +54,9 @@ const Dog = () => {
       <primitive
         object={model.scene}
         position={[0.25, -0.4, 0]}
-        rotation={[0, Math.PI / 3.5, 0]}
+        rotation={[0, Math.PI / 3.7, 0]}
       />
       <directionalLight position={[0, 2, 2]} color={0xffffff} intensity={10} />
-      <OrbitControls />
     </>
   );
 };
