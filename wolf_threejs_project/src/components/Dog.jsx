9a85606fbbe7f7d8b72139bf7e7bc7555b5b6955
raju barman <rajubarman7665@gmail.com>
2026-05-08 14:13:45 +0530

    3d models color added

--- wolf_threejs_project/src/components/Dog.jsx
+++ wolf_threejs_project/src/components/Dog.jsx
@@ -1,16 +1,47 @@
+import * as THREE from "three";
 import { useThree } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
 
 const Dog = () => {
   const { scene } = useGLTF("/models/dog.drc.glb");
-  useThree(({ camera, scene, gl })=>{
-    camera.position.z = 2;
-    
+
+  useThree(({ camera, scene, gl }) => {
+    camera.position.z = 1;
+    gl.toneMapping = THREE.ReinhardToneMapping;
+    gl.outputColorSpace = THREE.SRGBColorSpace;
   });
+
+  // const textures = useTexture({
+  //   normalMap: "dog_normals.jpg",
+  //   sampleCap: "/matcap/mat-2.png",
+  // });
+
+  const [normalMap, sampleCap] = useTexture([
+    "/dog_normals.jpg",
+    "/matcap/mat-2.png",
+  ]).map((texture) => {
+    texture.flipY = false;
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  });
+
+  scene.traverse((child) => {
+    if (child.name.includes("DOG")) {
+      child.material = new THREE.MeshMatcapMaterial({
+        normalMap: normalMap,
+        matcap: sampleCap,
+      });
+    }
+  });
+
   return (
     <>
-      <primitive object={scene} position={[0, 0, 0]}  />
-      <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
+      <primitive
+        object={scene}
+        position={[0.25, -0.4, 0]}
+        rotation={[0, Math.PI / 3.5, 0]}
+      />
+      <directionalLight position={[0, 2, 2]} color={0xffffff} intensity={10} />
       <OrbitControls />
     </>
   );
